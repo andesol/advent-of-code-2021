393af571d526d8301c193356cd21c7d7f29b58d1
andesol <anddessol@gmail.com>
2021-12-01 18:56:30 +0100

    add solution for part 2
    add gitignore
    
    formatting

--- day-1/main.py
+++ day-1/main.py
@@ -3,20 +3,38 @@ Day 1
 How many measurements are larger than the previous measurement?
 """
 
-count = 0
+with open("day-1/input.txt") as f:
+    numbers = [int(line) for line in f]
 
-with open('day-1/input.txt') as f:
-    for line in f:
-        number = int(line)        
+
+def count_increases(numbers):
+    count = 0
+    for n in numbers:
         try:
-            if number > previous:
+            if n > previous:
                 count += 1
-        except NameError as e:
+        except NameError:
             pass
-            
-        previous = number
+
+        previous = n
+
+    return count
 
 
-print(f"Count: {count}")
+# Part 1
+count1 = count_increases(numbers)
+print(f"Part 1: Count: {count1}")
 
 
+# Part 2
+measurements = []
+for i, item in enumerate(numbers):
+    try:
+        m = item + numbers[i + 1] + numbers[i + 2]
+    except:
+        pass
+
+    measurements.append(m)
+
+count2 = count_increases(measurements)
+print(f"Part 2: Count: {count2}")
